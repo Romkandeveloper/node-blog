baa78cb09f5934cae33eb0edc687333fbde24ac8
roman chaban <romanchaban9@gmail.com>
2023-05-05 18:36:48 +0300

    feat(login): add auth middleware

---
+++ middlewares/IsAuthMiddleware.js
@@ -0,0 +1,18 @@
+import jwt from 'jsonwebtoken';
+import {SECRET} from "../index.js";
+
+export default (req, res, next) => {
+    try {
+        const token = (req.headers.authorization || '').replace(/Bearer\s?/, '');
+        if (!token) throw ({code: 403, message: 'Forbidden.'})
+
+        const decodedToken = jwt.verify(token, SECRET);
+        req.userId = decodedToken._id;
+
+        next();
+    } catch (e) {
+        return res.status(e.code || 403).json({
+            message: e.message,
+        });
+    }
+}
